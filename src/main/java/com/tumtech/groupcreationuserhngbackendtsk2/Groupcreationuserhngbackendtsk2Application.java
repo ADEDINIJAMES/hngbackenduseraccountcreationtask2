b6c8eb13269a470984e3f8c1a3caef2b288556fc
James Adedini <145149678+ADEDINIJAMES@users.noreply.github.com>
2024-07-07 12:56:02 +0100

    first commit

---
+++ src/main/java/com/tumtech/groupcreationuserhngbackendtsk2/Groupcreationuserhngbackendtsk2Application.java
@@ -0,0 +1,13 @@
+package com.tumtech.groupcreationuserhngbackendtsk2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groupcreationuserhngbackendtsk2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Groupcreationuserhngbackendtsk2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tumtech/groupcreationuserhngbackendtsk2/Groupcreationuserhngbackendtsk2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tumtech.groupcreationuserhngbackendtsk2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Groupcreationuserhngbackendtsk2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
